55e65a7aa098db7934a4cf684c91dae68c20ae48
zk00332 <zk00332@surrey.ac.uk>
2024-06-11 00:36:22 +0100

    few fixes

--- ml/zayaans-ml/linear-regression-cost-function/LinearRegression.py
+++ ml/zayaans-ml/linear-regression-cost-function/LinearRegression.py
@@ -3,42 +3,53 @@ import matplotlib.pyplot as plt
 import math
 import time
 
+
 class LinearRegression:
     def __init__(self, X, Y):
         self.X = X
-        self.Y = Y # this is the result (not prediction)
-        self.b = [0, 0] # set w and b = 0 (y = wx + b)
+        self.Y = Y
+        self.wb = [0, 0] # w, b
 
     def predict(self, X=[]):
-        Y_prediction = np.array([]) # creating array for y-hat buffer
-        if not X: X = self.X # if X is not initialized
-        b = self.b
-        for x in X: 
-            Y_prediction = np.append(Y_prediction, b[0]) + (b[1] * x)
-        return Y_prediction # return y-hat buffer
-    
+        Y_prediction = np.array([])
+        if len(X) == 0: X = self.X # initialize X if not created
+        X = np.array(X)
+        wb = self.wb
+        for x in X:
+            Y_prediction = np.append(Y_prediction, wb[1]) + (wb[0] * x)
+        return Y_prediction # this is returning the y-hat buffer
+
+
     def update_coefficients(self, learning_rate):
-        Y_prediction = self.predict()
+        Y_prediction = self.predict(self.X)
         Y = self.Y
-        m = len(Y) # number of training samples
-        # now we update b and w using 
+        m = len(Y) # number of training samplesd
+        # for w
+        #self.wb[0] = self.wb[0] - (learning_rate * ((1 / m) * np.sum((Y_prediction - Y) * self.X)))
         # for b
-        self.b[0] = self.b[0] - (learning_rate * ((1 / m) * np.sum(Y_prediction - Y)))
-        self.b[1] = self.b[1] - (learning_rate * ((1 / m) * np.sum((Y_prediction - Y) * self.X)))
+        #self.wb[1] = self.wb[1] - (learning_rate * ((1 / m) * np.sum((Y_prediction - Y))))
+        
+        # Update for w
+        dw = (1 / m) * np.dot(X.T, (Y_prediction - Y))
+        self.wb[0] = self.wb[0] - learning_rate * dw
 
-    def get__current_accuracy(self, Y_prediction):
+        # Update for b
+        db = (1 / m) * np.sum(Y_prediction - Y)
+        self.wb[1] = self.wb[1] - learning_rate * db
+
+
+    def get_current_accuracy(self, Y_prediction):
         prediction, actual = Y_prediction, self.Y
-        n = len(Y_prediction) # number of predictions
+        ss_res = np.sum((actual - prediction) ** 2)  # Residual sum of squares
+        ss_tot = np.sum((actual - np.mean(actual)) ** 2)  # Total sum of squares
+        r_squared = 1 - (ss_res / ss_tot)
+        return r_squared
 
-        for i in range(n):
-            if actual[i] != 0:
-                return abs(1- np.sum(abs(prediction[i] - actual[i]) / actual[i]) / n)
-    
     def compute_loss(self, Y_prediction):
         m = len(self.Y)
-        J = (1 / 2 * m) * (np.sum((Y_prediction - self.Y) ** 2))
-        return J
-    
+        J_cost_function = (1 / 2 * m) * (np.sum((Y_prediction - self.Y) ** 2))
+        return J_cost_function
+
     def plot_best_fit(self, Y_prediction, figure):
         figureObject = plt.figure(figure)
         # create a scatter plot
@@ -46,51 +57,48 @@ class LinearRegression:
         # add the line in
         plt.plot(self.X, Y_prediction, color="g")
         figureObject.show()
-    
-if __name__ == "__main__":
-    # create X and Y
+
+if __name__== "__main__":
     X = np.array([i for i in range(11)])
     Y = np.array([2*i for i in range(11)])
+
+    print(Y)
     
     regressionModel = LinearRegression(X, Y)
+    
+    print(regressionModel)
 
-    iterations = 0 
+    iterations = 0
     steps = 100
-    learning_rate = 0.01
+    learning_rate = 0.0000001
     costs_buffer = []
 
-    # plot hte original best-fit line
     Y_prediction = regressionModel.predict()
     regressionModel.plot_best_fit(Y_prediction, "Initial Line of Best Fit")
 
     while 1:
         Y_prediction = regressionModel.predict()
-        # generate the cost on y-hat
+        # here we generate the cost on y-hat
         cost = regressionModel.compute_loss(Y_prediction)
-        # add that cost to the buffer
+        # add cost to buffer
         costs_buffer.append(cost)
         regressionModel.update_coefficients(learning_rate)
 
         iterations += 1
-        # get the epochs (cycles of iterations per step)
-        if iterations % steps == 0: 
-            print(iterations, "epochs elapsed")
-            print("Current accuracy is: ", regressionModel.get__current_accuracy(Y_prediction))
+        if iterations % steps == 0:
+            print("\n", iterations, "epochs elapsed")
+            print("Current accuracy is: ", regressionModel.get_current_accuracy(Y_prediction))
 
-            end = input("End run? y/n")
+            end = input("End run? y/n: ")
             if end == "y":
                 break
-    
-    regressionModel.plot_best_fit(Y_prediction, "Final Line of Best Fit")
-    time.sleep(5)
 
-    # Create a plot to see if the cost/loss function decreases
+    regressionModel.plot_best_fit(Y_prediction, "Final Line of Best Fit")
+    # time.sleep(10)
+
+    # creating a plot to see if the cost/loss function decreases
     plot = plt.figure("Verification")
     plt.plot(range(iterations), costs_buffer, color="b")
-    plot.show()
+    time.sleep(10)
 
-    # if the user wants to predict using the model
-    regressionModel.predict([i for i in range(10)])
-
-
-
+    print(regressionModel.predict([i for i in range(10)]))
