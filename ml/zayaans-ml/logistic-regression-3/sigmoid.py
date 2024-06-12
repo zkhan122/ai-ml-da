4890632ceb39f2262c228402b61d41a6dcf537e6
zk00332 <zk00332@surrey.ac.uk>
2024-06-12 15:45:22 +0100

    add

--- ml/zayaans-ml/logistic-regression-3/sigmoid.py
+++ ml/zayaans-ml/logistic-regression-3/sigmoid.py
@@ -1,5 +1,6 @@
 # sigmoid is a activation function used to map input between 0 and 1 then classification can be done based on decision boundary
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 
 def sigmoid(input):
@@ -17,3 +18,21 @@ outputs = [sigmoid(y) for y in inputs]
 print("Inputs: ", inputs)
 
 print("\nOutputs (Sigmoid applied): ", outputs)
+
+print("\n Applied to distributed array: ")
+z_tmp = np.arange(-10, 11)
+out_tmp = sigmoid(z_tmp)
+print(out_tmp)
+
+np.set_printoptions(precision=3) 
+print("Input (z), Output (sigmoid(z))")
+print(np.c_[z_tmp, out_tmp])
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
+ax.plot(z_tmp, out_tmp, c="b")
+ax.set_title("Sigmoid function")
+ax.set_ylabel('sigmoid(z)')
+ax.set_xlabel('z')
+ax.axvline(x=0, color='r', linestyle='--', label=f'Threshold = {0}')
+ax.legend()
+plt.show()
