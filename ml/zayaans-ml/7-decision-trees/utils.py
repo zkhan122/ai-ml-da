574ab476ed43c308b73cfbcf2e2e9a5d83778fad
Zayaan <zayaankashif@yahoo.com>
2025-01-28 22:47:16 +0000

    push

--- ml/zayaans-ml/7-decision-trees/utils.py
+++ ml/zayaans-ml/7-decision-trees/utils.py
@@ -2,94 +2,6 @@ from PIL import Image
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.drawing.nx_pydot import graphviz_layout
-import numpy as np
-from matplotlib.widgets import Slider, Button
-plt.style.use('./deeplearning.mplstyle')
-
-def compute_entropy(y):
-
-    entropy = 0
-    
-    if len(y) == 0:
-        return 0
-    entropy = sum(y[y==1])/len(y)
-    if entropy == 0 or entropy == 1:
-        return 0
-    else:
-        return -entropy*np.log2(entropy) - (1-entropy)*np.log2(1-entropy)
-     
-
-def split_dataset(X, node_indices, feature):
-
-    left_indices = []
-    right_indices = []
-
-    for i in node_indices:
-        if X[i][feature] == 1:
-            left_indices.append(i)
-        else:
-            right_indices.append(i)
-        
-    return left_indices, right_indices   
-    
-    
-
-def compute_information_gain(X, y, node_indices, feature):
-    
-    left_indices, right_indices = split_dataset(X, node_indices, feature)
-    
-    X_node, y_node = X[node_indices], y[node_indices]
-    X_left, y_left = X[left_indices], y[left_indices]
-    X_right, y_right = X[right_indices], y[right_indices]
-    
-    information_gain = 0
-    
-    node_entropy = compute_entropy(y_node)
-    left_entropy = compute_entropy(y_left)
-    right_entropy = compute_entropy(y_right)
-    w_left = len(X_left) / len(X_node)
-    w_right = len(X_right) / len(X_node)
-    weighted_entropy = w_left * left_entropy + w_right * right_entropy
-    information_gain = node_entropy - weighted_entropy
-    
-    return information_gain
-
-def get_best_split(X, y, node_indices):   
-    num_features = X.shape[1]
-    
-    best_feature = -1
-
-    max_info_gain = 0
-    for feature in range(num_features):
-        info_gain = compute_information_gain(X, y, node_indices, feature)
-        if info_gain > max_info_gain:
-            max_info_gain = info_gain
-            best_feature = feature
-        
-   
-    return best_feature
-
-
-def build_tree_recursive(X, y, node_indices, branch_name, max_depth, current_depth, tree):
-
-    if current_depth == max_depth:
-        formatting = " "*current_depth + "-"*current_depth
-        print(formatting, "%s leaf node with indices" % branch_name, node_indices)
-        return
-   
-
-    best_feature = get_best_split(X, y, node_indices) 
-    
-    formatting = "-"*current_depth
-    print("%s Depth %d, %s: Split on feature: %d" % (formatting, current_depth, branch_name, best_feature))
-    
-
-    left_indices, right_indices = split_dataset(X, node_indices, best_feature)
-    tree.append((left_indices, right_indices, best_feature))
-    
-    build_tree_recursive(X, y, left_indices, "Left", max_depth, current_depth+1, tree)
-    build_tree_recursive(X, y, right_indices, "Right", max_depth, current_depth+1, tree)
-    return tree
 
 def generate_node_image(node_indices):
     image_paths = ["images/%d.png" % idx for idx in node_indices]
@@ -132,7 +44,7 @@ def generate_split_viz(node_indices, left_indices, right_indices, feature):
     trans=ax.transData.transform
     trans2=fig.transFigure.inverted().transform
 
-    feature_name = ["Ear Shape", "Face Shape", "Whiskers"][feature]
+    feature_name = ["Brown Cap", "Tapering Stalk Shape", "Solitary"][feature]
     ax_name = ["Splitting on %s" % feature_name , "Left: %s = 1" % feature_name, "Right: %s = 0" % feature_name]
     for idx, n in enumerate(G):
         xx,yy=trans(pos[n]) # figure coordinates
@@ -159,8 +71,8 @@ def generate_tree_viz(root_indices, y, tree):
     
     num_images = [len(root_indices)]
     
-    feature_name = ["Ear Shape", "Face Shape", "Whiskers"]
-    y_name = ["Non Cat","Cat"]
+    feature_name = ["Brown Cap", "Tapering Stalk Shape", "Solitary"]
+    y_name = ["Poisonous","Edible"]
     
     decision_names = []
     leaf_names = []
@@ -201,36 +113,6 @@ def generate_tree_viz(root_indices, y, tree):
         a.set_aspect('equal')
         a.imshow(G.nodes[n]['image'])
         a.axis('off')
-        try:
-            a.set_title(node_names[idx], y=-0.8, fontsize=13, loc="left")
-        except:
-            pass
+        a.set_title(node_names[idx], y=-0.8, fontsize=13, loc="left")
     ax.axis('off')
     plt.show()
-
-def plot_entropy():
-    def entropy(p):
-        if p == 0 or p == 1:
-            return 0
-        else:
-            return -p * np.log2(p) - (1- p)*np.log2(1 - p)
-    p_array = np.linspace(0,1,201)
-    h_array = [entropy(p) for p in p_array]
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(left=0.25, bottom=0.25)
-    ax.set_title('p x H(p)')
-    ax.set_xlabel('p')
-    ax.set_ylabel('H(p)')
-    axfreq = plt.axes([0.25, 0.1, 0.65, 0.03])
-    h_plot = ax.plot(p_array,h_array)
-    scatter = ax.scatter(0,0,color = 'red', zorder = 100, s = 70)
-    slider = Slider(axfreq, 'p', 0, 1, valinit = 0, valstep = 0.05)
-
-    def update(val):
-        x = val
-        y = entropy(x)
-        scatter.set_offsets((x,y))
-
-    slider.on_changed(update)
-    return slider
-    #plt.plot()
